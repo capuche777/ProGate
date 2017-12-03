15005feb6c76076348a0a224f104377381f9dafa
Jeremias Enriquez <capuche777@gmail.com>
2017-12-03 17:59:00 -0600

    jQuery lesson 1 has been finished

---
+++ jQuery/03-findingElements/10-findAndChildren/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('#find-method').click(function () {
+        $('#wrapper').find('a').css('color', 'red');
+    });
+    $('#children-method').click(function () {
+        $('#wrapper').children('a').fadeOut(1500);
+    });
+});
